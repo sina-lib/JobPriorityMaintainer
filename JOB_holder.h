562102f45c283afdbe39b7c0e791ff0a4496b083
sina-lib <mszandian@aut.ac.ir>
2020-02-25 23:26:26 +0000

    only shows current time and then exits

--- JOB_holder.h
+++ JOB_holder.h
@@ -1,3 +1,11 @@
 
 #define JobPM_VERSION_MAJOR 0
 #define JobPM_VERSION_MINOR 5
+
+class JOB_holder
+{
+public:
+	JOB_holder();
+	~JOB_holder();
+private:
+};
--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <chrono>
+#include "JOB_holder.h"
+#include <iomanip>
+#include <chrono>
 
 
 int main(int argc, char* argv[])
 {
 	std::cout << "this is a cmake test" << std::endl;
+	
+
+	std::time_t nt = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+
+	std::cout << std::put_time(std::localtime(&nt),"%c") << std::endl;
+	
 	return 0;
 }
