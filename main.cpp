cdf5ed9c348596a7c77a59d4d6af73cb605b6051
sina-lib <mszandian@aut.ac.ir>
2020-04-17 23:24:42 +0100

    find and load jobs

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 #include <chrono>
 #include "Validator.hpp"
 #include "TimeUtility.h"
+#include <stdlib.h>
+#include <boost/filesystem.hpp>
+#include <sstream>
 
 inline void print(const std::string& matn)
 {
@@ -12,11 +15,39 @@ inline void print(const std::string& matn)
 }
 #include "JOB_test_helper.cpp"
 
+using namespace boost::filesystem;
+
 int main(int argc, char* argv[])
 {
 
-	
-	
+	// print a simple size
+	std::cout << "size of JPM exec is " << file_size("./JPM") << " bytes" << std::endl;
+
+	// goto a folder
+	path p("rjobs");
+	if (exists(p))
+	{
+		if (is_regular_file(p))
+			std::cout << "p is a file!" << std::endl;
+		else
+		{
+			std::cout << "contents of p: " << std::endl;
+			for (directory_entry& x : directory_iterator(p))
+			{
+				std::cout  << "    " << x.path() << '\n';
+
+				std::ostringstream name{};
+				name << x.path();
+
+				std::string p  = name.str();
+				p.erase(p.begin());
+				p.erase(p.end()-1);
+				
+				JOB_holder a{p};
+				a.prettyPrintThisJob();
+			}
+		}
+	}
 	
 	
 	std::cout << "SiNa" << std::endl;
